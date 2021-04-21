163183e9be7eb0f25e3e2ff51a82c3d7405299fd
I523036 <tobias.joschko@sap.com>
2021-04-21 18:03:37 +0200

    neue Zeile

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main () {
   std::cout << "Hello World!\n";
   std::cout << "This is the starting file for your Todo list\n";
   std::cout << "Include the relevant header files here to run\n";
+  std::cout << "neue Zeile\n";
 
   return EXIT_SUCCESS;
 }
